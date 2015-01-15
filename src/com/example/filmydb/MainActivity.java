662c1c4a7081b9f5446ea061653db85ba113e2a5
Sysfun <Sysfunn@gmail.com>
2015-01-15 12:01:02 +0100

    test

--- src/com/example/filmydb/MainActivity.java
+++ src/com/example/filmydb/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-		/// zatial nic
+		/// zatial tu nic nie je
 	}
 
 	@Override
